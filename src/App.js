e524a607ea696900b4eb2a43a52573ea8a043f05
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-02-22 12:30:40 +0100

    fucked up versions but ok

--- src/App.js
+++ src/App.js
@@ -10,24 +10,34 @@ function App() {
   const dates = {startDate: new Date('February 18, 2022').toISOString(), endDate: new Date('February 20, 2024').toISOString()}
   const queryString = new URLSearchParams(dates)
 
+
+  let newsData;
+
   // Fetch news from API
   fetch(`${apiUrl}?${queryString}`)
     .then(response => {
+    // Check response
       if (!response.ok) {
         throw new Error('Network response not 200');
       }
       return response.json();
     })
-    .then((data) => console.log(data))
+    // Count labels grouped by region.
+    .then((data) => {
+      const labelCount = Geomath.countLabels(data);
+      const regionRatios = Geomath.getOdds(labelCount);
+      console.log(regionRatios);
+    }
+    )
     .catch((error) => {
       console.error('Error:', error);
     });
+  
+
 
 
   return (
-    <div className="big-button">
-      <button></button>
-    </div>
+    <Geomap newsdata={newsData}/>
   )
 }
 
---
+++ src/Geomap.js
@@ -0,0 +1,54 @@
+import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+// import { scaleLinear } from "d3-scale";
+import { useState, useEffect } from "react";
+
+// Get topoJSONdata from local folder.
+import continents from './data/continents.json';
+
+// YOU GOTTA MAP THE ODDS FROM THE NEWS ONTO THE CORRECT FUCKINT PROPERTY OKAY?? THGEN JUST GO 
+
+// Set color scale
+// const colorScale = scaleLinear().domain([0, 100]).range(['#FFF', "#06F"])
+
+const Geomap = ( {onFetchData} ) => {
+  const [queryData, setQueryData] = useState({});
+
+  // This gets sets query data. If called on its own it will call forever because promise fullfillment changes state.
+  async function handleFetchData() {
+    const queryData = await onFetchData();
+    if (queryData){
+      setQueryData(queryData);
+    } else {
+      console.log("Fetch error, data not found")
+    }
+  }
+
+  // Call handleFetchData once.
+  useEffect(() => {handleFetchData()}, [])
+
+  // Get the queryData to MAP to ech region somehow. 
+  // const regionData = continents.map((region) => {
+  //   console.log(region.objects)
+  // })
+
+  return (
+      <ComposableMap>
+        <Geographies geography={continents}>
+        {({ geographies }) =>
+  geographies.map((geo) => {
+    return (
+      <Geography
+        key={geo.rsmKey}
+        geography={geo}
+      //   fill={colorScale(geo.properties.THECORRECTFUCKINGVALUE)}
+      />
+    )
+  })
+}
+        </Geographies>
+      </ComposableMap>
+    )
+  
+}
+ 
+export default Geomap;
