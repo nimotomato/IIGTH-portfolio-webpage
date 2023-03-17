ae07bff1251fd6328134997b92720aaaa335e566
Nimotomato <108477753+nimotomato@users.noreply.github.com>
2023-03-17 09:31:45 +0100

    cleint working fine

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,44 @@
-import '../css/App.css';
+import "../css/App.css";
 
-
-import Geomap from './Geomap';
-import Datepicker from './Datepicker';
-import Legend from './Legend';
-import Description from './Description';
-
+import Geomap from "./Geomap";
+import Datepicker from "./Datepicker";
+import Legend from "./Legend";
+import Description from "./Description";
 
 import { useState, useEffect } from "react";
-import fetchData from '../helpers/fetchData';
-
+import fetchData from "../helpers/fetchData";
 
 function App() {
   // The local endpoint we run the express API on
-  const apiUrl = "https://73hflv8mr2.execute-api.eu-north-1.amazonaws.com/default/api/";
-
+  const apiUrl = "http://localhost:5000/api/";
 
   // Theme for map and legend
   const scaleColors = {
     scale: [0.2, 0.6],
-    colors: ["linen", "coral"]
+    colors: ["linen", "coral"],
   };
-  
 
   // States for the dates used in API search query
   const [chosenDates, setChosenDates] = useState();
 
-
   // State for dates used in min/max legend values
   const [minMaxDates, setMinMaxDates] = useState();
 
-
   // State for news data
   const [data, setData] = useState({});
-
 
   // Get date min/max range
   async function handleFetchDates() {
     try {
       const response = await fetch(`${apiUrl}dates`);
       const data = await response.json();
+
       // Create the default days (min, max) for the query string
-      const startDate = data[0].startDate.split("T")[0];
-      const endDate = data[0].endDate.split("T")[0];
+      const startDate = data.startDate.split("T")[0];
+      const endDate = data.endDate.split("T")[0];
+
       setChosenDates([startDate, endDate]);
-      setMinMaxDates({startDate: startDate, endDate: endDate});
+      setMinMaxDates({ startDate: startDate, endDate: endDate });
     } catch (e) {
       console.log(e);
     }
@@ -52,49 +46,45 @@ function App() {
 
   // Fetch date range on component load
   useEffect(() => {
-    handleFetchDates()}, []);
+    handleFetchDates();
+  }, []);
 
-  
   // Fetch new data on updated dates
   useEffect(() => {
     fetchData(apiUrl, chosenDates)
-      .then(result => {
-        if(result){
+      .then((result) => {
+        if (result) {
           setData(result);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-    }, [chosenDates]);
-
+  }, [chosenDates]);
 
   // Handle chosen dates, used in DATEPICKER component
   const handleChosenDates = (dates) => {
     setChosenDates(dates);
-  } 
-
+  };
 
   return (
     <div className="content-container">
       <h1 className="main-title">Is it going to hell?</h1>
       <Description />
-        <Geomap 
-          data={data} 
-          theme={scaleColors}/>
+      <Geomap data={data} theme={scaleColors} />
       {/* Make sure date picker is only loaded after dates have been fetched */}
       <div className="date-legend-container">
-        {chosenDates && <Datepicker 
-          chosenDates={chosenDates} 
-          onChose={handleChosenDates} 
-          minMaxDates={minMaxDates}/>}      
-        <Legend 
-          data={data} 
-          theme={scaleColors}/>
-        </div>
+        {chosenDates && (
+          <Datepicker
+            chosenDates={chosenDates}
+            onChose={handleChosenDates}
+            minMaxDates={minMaxDates}
+          />
+        )}
+        <Legend data={data} theme={scaleColors} />
+      </div>
     </div>
   );
 }
-
 
 export default App;
--- src/helpers/fetchData.js
+++ src/helpers/fetchData.js
@@ -1,47 +1,43 @@
-import { Geomath } from './Geomath';
+import { Geomath } from "./Geomath";
 
 // Set query string
 const getQueryString = (apiUrl, chosenDates) => {
-    if (chosenDates && chosenDates[0] && chosenDates[1]){
-        const dateQuery = new URLSearchParams({startDate: chosenDates[0], endDate: chosenDates[1]});
-        return (`${apiUrl}news?${dateQuery}`);
-    }else {
-        return null;
-    }
-}
-
+  if (chosenDates && chosenDates[0] && chosenDates[1]) {
+    const dateQuery = new URLSearchParams({
+      startDate: chosenDates[0],
+      endDate: chosenDates[1],
+    });
+    console.log(`${apiUrl}news?${dateQuery}`);
+    return `${apiUrl}news?${dateQuery}`;
+  } else {
+    return null;
+  }
+};
 
 // Calculate results (odds or percentage) based on the query data
 async function parseData(queryString) {
-    const response = await fetch(`${queryString}`);
+  const response = await fetch(`${queryString}`);
 
+  const data = await response.json();
 
-    const data = await response.json();
-
-
-    if (data){
-        return Geomath.getPercentage(data);
-    } 
+  if (data) {
+    return Geomath.getPercentage(data);
+  }
 }
-
 
 // Active function
-const withFetchNews = async (apiUrl, chosenDates) => {
-    const queryString = getQueryString(apiUrl, chosenDates)
+const fetchData = async (apiUrl, chosenDates) => {
+  const queryString = getQueryString(apiUrl, chosenDates);
 
+  if (!queryString) {
+    return null;
+  }
 
-    if (!queryString){
-        return null
-    }
+  const regionResults = await parseData(queryString).catch((error) => {
+    console.log(error.message);
+  });
 
+  return regionResults;
+};
 
-    const regionResults = await parseData(queryString)
-        .catch(error => {
-            console.log(error.message);
-        });
-
-
-    return regionResults;
-}
- 
-export default withFetchNews;
+export default fetchData;
